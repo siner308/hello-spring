0effc09e58d233a4d6f34227b1408f17fbe59b49
siner308 <siner308@gmail.com>
2022-02-10 22:46:22 +0900

    스프링 웹 개발 기초 까지 작성 완료

---
+++ src/main/java/hello/hellosiner/HelloSinerApplication.java
@@ -0,0 +1,13 @@
+package hello.hellosiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hellosiner/HelloSinerApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellosiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
