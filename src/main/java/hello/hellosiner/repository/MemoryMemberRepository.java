4579912b534476fd1fea19923bd5e4e10b434755
siner308 <siner308@gmail.com>
2022-02-11 15:30:24 +0900

    member repo 추가 및 테스트코드 작성

---
+++ src/main/java/hello/hellosiner/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package hello.hellosiner.repository;
+
+import hello.hellosiner.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store = new HashMap<>();
+    }
+}
