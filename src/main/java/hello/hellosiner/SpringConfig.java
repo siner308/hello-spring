19e993560e22aa2a5842b563cf9e44bc6c938c33
siner308 <siner308@gmail.com>
2022-02-12 21:04:25 +0900

    JdbcTemplateRepository 추가

--- src/main/java/hello/hellosiner/SpringConfig.java
+++ src/main/java/hello/hellosiner/SpringConfig.java
@@ -1,14 +1,27 @@
 package hello.hellosiner;
 
 import hello.hellosiner.domain.Member;
+import hello.hellosiner.repository.JdbcMemberRepository;
+import hello.hellosiner.repository.JdbcTemplateMemberRepository;
 import hello.hellosiner.repository.MemberRepository;
 import hello.hellosiner.repository.MemoryMemberRepository;
 import hello.hellosiner.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +29,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(this.dataSource);
     }
 }
