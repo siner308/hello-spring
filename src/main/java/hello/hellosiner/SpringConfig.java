4f54ffc233b4b4a0de2f3247e88d6cf71ff55ba0
siner308 <siner308@gmail.com>
2022-02-12 22:00:46 +0900

    SpringDataJpa 추가

--- src/main/java/hello/hellosiner/SpringConfig.java
+++ src/main/java/hello/hellosiner/SpringConfig.java
@@ -1,32 +1,21 @@
 package hello.hellosiner;
 
-import hello.hellosiner.domain.Member;
 import hello.hellosiner.repository.*;
 import hello.hellosiner.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(this.em);
+        return new MemberService(memberRepository);
     }
 }
---
+++ src/main/java/hello/hellosiner/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package hello.hellosiner.repository;
+
+import hello.hellosiner.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
